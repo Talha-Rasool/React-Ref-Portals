a2e02baa24298278949c5a53e746f909cda55b13
Talha-Rasool <talharasool450@gmail.com>
2024-05-26 08:23:42 +0200

    `feat`: enhance TimerChallenge component with setInterval for real-time updates
    - Replace setTimeout with setInterval for continuous updates on time remaining
    - Improve functionality to display real-time information about time remaining
    - Enhance user experience by providing live updates during the timer countdown"

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,41 +2,55 @@ import { useState, useRef } from "react";
 import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
+  //   const [timeStarted, setTimeStarted] = useState(false);
+  //   const [timeExpiried, setTimeEXpiried] = useState(false);
+  // setTimeout(() => {
+  //     setTimeEXpiried(true);
+  //     modal.current.show();
+  //   }, targetTime * 1000);
+  //   setTimeStarted(true);
+
   const timer = useRef();
-  const modal = useRef();
+  const dialog = useRef();
 
-  const [timeStarted, setTimeStarted] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const timeIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
-  const [timeExpiried, setTimeEXpiried] = useState(false);
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    setTimeRemaining(targetTime * 1000); //Not Ideal to reset state. But due to If statement Its possible
+    dialog.current.open();
+
+        
+  }
 
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimeEXpiried(true);
-      modal.current.show();
-    }, targetTime * 1000);
-    setTimeStarted(true);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimie) => prevTimie - 10);
+    }, 10);
   }
 
   function handleFinish() {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
+   
   }
   return (
     <>
-      {timeExpiried && (
-        <ResultModal ref={modal} targetTime={targetTime} result="lost" />
-      )}
+       <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timeStarted ? handleFinish : handleStart}>
-            {timeStarted ? "Stop": "Start"} 
+          <button onClick={timeIsActive ? handleFinish : handleStart}>
+            {timeIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timeStarted ? "active" : undefined}>
-          {timeStarted ? "Time is running.." : "Timer inactive"}
+        <p className={timeIsActive ? "active" : undefined}>
+          {timeIsActive ? "Time is running.." : "Timer inactive"}
         </p>
       </section>
     </>
